942e17fd3783a6df375785c0f14ecab95061ae66
Сасмиль Кирилл Александрович <kir.sasmil2002@gmail.com>
2022-02-14 14:48:01 +0300

    work with static

--- products/views.py
+++ products/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def index(request):
     return render(request, 'products/index.html')
 
+
 def products(reauest):
     return render(reauest, 'products/products.html')
-
 
 
 def test_context(request):
