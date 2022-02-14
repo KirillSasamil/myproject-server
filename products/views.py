fa427b85d4e41002db3baa37b1d1d5daa979f7bb
Сасмиль Кирилл Александрович <kir.sasmil2002@gmail.com>
2022-02-14 14:22:27 +0300

    added test_context() controller

--- products/views.py
+++ products/views.py
@@ -7,3 +7,19 @@ def index(request):
 
 def products(reauest):
     return render(reauest, 'products/products.html')
+
+
+
+def test_context(request):
+    context = {
+        'title': 'GeekShop - Тестовый контекст',
+        'header': 'Welcome!',
+        'username': 'Иван Иванов',
+        'products': [
+            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090},
+            {'name': 'Синяя куртка The North Face', 'price': 23725},
+            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390},
+        ],
+        'is_promotion': False,
+    }
+    return render(request, 'products/test-context.html', context)
